4e55994f4f00d829366aec7147e5204c1dd0c8e6
DingusDungus <DingusDungus@github.com>
2021-11-10 23:27:16 +0000

    Finished part 3

--- part3.s
+++ part3.s
@@ -9,22 +9,25 @@
 
 facultyCalc:
   PUSH {lr}
+  MOV r0, r5
   MOV r1, r0
   MOV r3, r0
   SUB r1, r1, #1
-again:
+recursive:
   CMP r1, #0
-  BEQ finish
+  BEQ base1
   MUL r0, r0, r1
   SUB r1, r1, #1
-  BAL again
-finish:
+  BAL recursive
+base1:
   MOV r4, r0
   POP {pc}
 
 main:
   PUSH {lr}
-  MOV r0, #4
+  MOV r5, #0
+loop:
+  ADD r5, r5 ,#1
   BL facultyCalc
   MOV r1, r3
   LDR r0, =writeFaculty
@@ -32,5 +35,7 @@ main:
   mov r1, r4
   LDR r0, =writeFaculty1
   BL printf
+  CMP r5, #10
+  BLT loop
   POP {pc}
 .end
